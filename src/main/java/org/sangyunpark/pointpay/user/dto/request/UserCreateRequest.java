83d1782688ae1a830423c9c9894bbf94262c6c62
sangyunpark99 <psangyun5108@gmail.com>
2025-08-30 10:49:09 +0900

    feat : 회원 생성시 사용되는 UserCreateRequest dto 구현

---
+++ src/main/java/org/sangyunpark/pointpay/user/dto/request/UserCreateRequest.java
@@ -0,0 +1,28 @@
+package org.sangyunpark.pointpay.user.dto.request;
+
+import jakarta.validation.constraints.*;
+
+public class UserCreateRequest {
+
+    @NotBlank
+    @Size(min = 3, max = 10)
+    private String username;
+
+    @NotNull
+    @Min(value = 0)
+    @Max(value = 2_100_000_000)
+    private int pointBalance;
+
+    public UserCreateRequest(final String username, final int pointBalance) {
+        this.username = username;
+        this.pointBalance = pointBalance;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public int getPointBalance() {
+        return this.pointBalance;
+    }
+}
