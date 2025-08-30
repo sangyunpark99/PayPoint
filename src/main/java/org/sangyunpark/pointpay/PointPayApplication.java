51740197abe0426452caa3ccc96a4e2161cdbc88
sangyunpark99 <psangyun5108@gmail.com>
2025-08-30 10:46:12 +0900

    feat: Spring 실행에 필요한 main 메서드 구현

---
+++ src/main/java/org/sangyunpark/pointpay/PointPayApplication.java
@@ -0,0 +1,13 @@
+package org.sangyunpark.pointpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointPayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PointPayApplication.class, args);
+    }
+
+}
