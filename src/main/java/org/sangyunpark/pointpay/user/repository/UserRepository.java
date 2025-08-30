0efe2d6d8367ad00add77e942e757f6a24ac88d5
sangyunpark99 <psangyun5108@gmail.com>
2025-08-30 10:49:36 +0900

    feat : 회원이름을 기준으로 존재 여부를 파악하는 메서드 구현

---
+++ src/main/java/org/sangyunpark/pointpay/user/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package org.sangyunpark.pointpay.user.repository;
+
+import org.sangyunpark.pointpay.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByUsername(String username);
+}
